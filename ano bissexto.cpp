dad911a3fa481643524de07590beaf0b500b3dda
JoaoGabrielMilare <168237403+JoaoGabrielMilare@users.noreply.github.com>
2024-08-31 13:48:55 -0300

    ano bissexto.cpp

--- ano bissexto.cpp
+++ ano bissexto.cpp
@@ -1,20 +1,19 @@
 #include <stdio.h>
-	int main(){
-		int ano;
-		char nome [99];
-printf("Digite o seu nome: ");
-scanf("%s", &nome);
-printf("Digite o ano que voce nasceu: ");
-scanf("%d", &ano);
-	if(ano % 4 == 0){
-		if(ano % 100 ==0){
-			if(ano % 400 ==0){
-				printf(" ola %s, seu ano é bissexto",nome);
-			}
-		}
-	}else{
-		printf("ola %s, voce nao nasceu em ano bissexto", nome);
-	}
-		
-		return 0;
-	}
+
+int main() {
+    int ano;
+    char nome[99];
+
+    printf("Digite o seu nome: ");
+    scanf("%s", nome); 
+
+    printf("Digite o ano que voce nasceu: ");
+    scanf("%d", &ano);
+
+    if (ano % 4 == 0 && (ano % 100 != 0 || ano % 400 == 0)) {
+        printf("Ola %s, seu ano e bissexto.\n", nome);
+    } else {
+        printf("Ola %s, voce nao nasceu em ano bissexto.\n", nome);
+    }
+    return 0;
+}
